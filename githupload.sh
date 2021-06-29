89314ddd6794a0969703edfa0966f5d067e10a46
Miguel <miguel.korn@hva.nl>
2021-06-29 11:19:19 +0100

    backup.2021-06-29.11:19:18

---
+++ githupload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+uploadbool=false
+today=`date +%Y-%m-%d.%H:%M:%S`
+backupdir="backups/"
+
+cd ~/klipper_config
+
+git pull origin Voron_V2.576
+
+for file in *[0-9].cfg; do
+    if [ -f "$file" ] # does file exist?
+        then
+            mv $file $backupdir$file
+            uploadbool=true
+    fi
+done
+
+git add .
+git commit -m "backup."$today
+git push
